95e61c7b71fdc8475cb4f3c1757b67361d5b8f6f
lstriegher <lstriegher@LSTRIE-WORKSTN>
2025-08-19 17:27:53 +0800

    Add project files.

---
+++ Docker Labs/docker_labs.cpp
@@ -0,0 +1,12 @@
+﻿// Docker Labs.cpp : Defines the entry point for the application.
+//
+
+#include "Docker Labs.h"
+#include <curl
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
