de5bd2e5eada38ffbdc36c29ddd1837eac72e558
Laith Striegher <lstriegher@striegher.dev>
2025-08-21 12:48:56 +0800

    Working GET AND PUT curl wrappers.

--- Docker Labs/src/curl_wrapper.h
+++ Docker Labs/src/curl_wrapper.h
@@ -6,12 +6,10 @@ class Curl_Wrapper {
 public:
 	Curl_Wrapper();
 	~Curl_Wrapper();
-	std::string Get(const std::string& url, const std::vector<std::string>& headers);
+	std::string Get(const std::string& url, std::vector<std::string>& headers);
 	std::string Post();
 	std::string Put(const std::string& url, const std::string& data, const std::vector<std::string>& headers);
-private:
+	static std::size_t WriteCallback(char* contents, std::size_t size, std::size_t nmemb, std::string* userp);
 	CURL* curl;
 	CURLcode res;
-	static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
-
 };
