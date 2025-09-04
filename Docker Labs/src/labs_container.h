bc17f57e1d3a3dada4f502c4301249b3580a6f32
Laith J. Striegher <lstriegher@striegher.dev>
2025-09-04 12:11:49 +0800

    Merged lab init, rm and nuke into labs_global.

--- Docker Labs/src/labs_container.h
+++ Docker Labs/src/labs_container.h
@@ -10,49 +10,28 @@ using json = nlohmann::json;
 namespace Docker_Labs {
 
 	class Container {
-		public:
-			explicit Container(std::string id);
-			//Container(std::string container_name, std::string image_name);
+	public:
+		explicit Container(std::string id);
 
-			//Accessors
-			std::string Get_ID();
-			//std::string Get_Name();
-			//std::string Get_Image();
-			//std::string Get_IP();
-			//std::vector<std::string> Get_Networks();
-			//bool Get_Status();
-			//
-			//Cache Accessors
-			std::string Get_Name_Cache();
-			std::string Get_Image_Cache();
-			std::string Get_IP_Cache();
-			std::vector<std::string> Get_Networks_Cache();
-			//
-			int Cache_Update();
-			static Container Bogus(std::string id, std::string name, std::string image, std::string ip, std::vector<std::string> networks);
-			//
-			////Controls
-			//int Start();
-			//int Stop();
-			//int Restart();
-			//int Kill();
-			//int Remove();
-			//json CallDockerAPI(const std::string& url, const std::string& data, std::string method);
-			//int Test_API();
-			//Docker_Labs::Curl_Wrapper curl;
+		//Accessors
+		std::string Get_ID();
+		//Cache Accessors
+		std::string Get_Name_Cache();
+		std::string Get_Image_Cache();
+		std::string Get_IP_Cache();
+		std::vector<std::string> Get_Networks_Cache();
+		int Cache_Update();
+		static Container Bogus(std::string id, std::string name, std::string image, std::string ip, std::vector<std::string> networks);
+
 	private:
-			
-			
-			std::string id;
-			std::string name_cache;
-			std::string image_cache;
-			std::string ip_cache;
-			std::vector<std::string> networks_cache;
-			
-	};
 
-	//namespace Docker::Commands {
-	//	Container Get_Container(std::string container_name);
-	//}
+
+		std::string id;
+		std::string name_cache;
+		std::string image_cache;
+		std::string ip_cache;
+		std::vector<std::string> networks_cache;
+
+	};
 
 }
---
+++ Docker Labs/src/labs_global.h
@@ -0,0 +1,10 @@
+#include "cloudflare_hook.h"
+#include "docker_hook.h"
+
+namespace Docker_Labs {
+	int Global_Handler(Command_Interpreter command, int argc, char* argv[]);
+	int Init_Handler(Docker_Labs::Cloudflare::Cloudflared cloudflared, Docker_Labs::Docker::Docker docker, std::string email, std::string image);
+	int Init_Handler(Docker_Labs::Cloudflare::Cloudflared cloudflared, Docker_Labs::Docker::Docker docker, std::string email, std::string image, std::string container_name);
+	int Rm_Handler(Docker_Labs::Cloudflare::Cloudflared cloudflared, Docker_Labs::Docker::Docker docker, std::string container_name, bool keep_container);
+	int Nuke(Docker_Labs::Cloudflare::Cloudflared cloudflared, Docker_Labs::Docker::Docker docker, bool keep_containers);
+}
